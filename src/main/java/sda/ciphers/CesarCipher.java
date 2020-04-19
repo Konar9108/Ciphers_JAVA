9f39dbd62016e86db99f287d359d3d544b0fce6b
Damian Konarowski <konar9108@gmail.com>
2020-04-19 13:18:20 +0200

    start

---
+++ src/main/java/sda/ciphers/CesarCipher.java
@@ -0,0 +1,87 @@
+package sda.ciphers;
+
+public class CesarCipher {
+    private String base;
+    private String encoded;
+    private String decoded;
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public CesarCipher(String toEncode) {
+        base = toEncode;
+    }
+
+
+    public CesarCipher() {
+        base = "";
+        encoded= "";
+        decoded= "";
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public String getEncode() {
+        return encoded;
+    }
+    public void decode() {
+        StringBuilder tempDecode = new StringBuilder();
+        for (int i = 0; i < base.length(); i++) {
+            char letter = base.charAt(i);
+            final boolean isLower = Character.isLowerCase(letter);
+            if (!isLower){
+                letter = Character.toLowerCase(letter);
+            }
+            final int position = alphabet.indexOf(letter);
+            final char c = alphabet.charAt(getDecodedPosition(position));
+            tempDecode.append(isLower ? c : Character.toUpperCase(c)); // pisanie if jako elvis, krocej
+        }
+        decoded = tempDecode.toString();
+
+    }
+
+
+    public void encode() {
+        StringBuilder tempEncode = new StringBuilder();
+        for (int i = 0; i < base.length(); i++) {
+            char letter = base.charAt(i);
+
+            if(!Character.isLetter(letter)) continue;
+
+            final boolean isLower = Character.isLowerCase(letter);
+            if (!isLower){
+                letter = Character.toLowerCase(letter);
+            }
+            final int position = alphabet.indexOf(letter);
+            final char c = alphabet.charAt(getEncodedPosition(position));
+            tempEncode.append(isLower ? c : Character.toUpperCase(c)); // pisanie if jako elvis, krocej
+        }
+        encoded = tempEncode.toString();
+    }
+
+    private int getEncodedPosition(int position) {
+        return (position + 3) % 26;
+    }
+
+    private int getDecodedPosition(int position) {
+                return (position +23) % 26;
+    }
+
+    public String getDecoded() {
+        return decoded;
+    }
+
+
+
+
+//TODO
+    /*
+     * przyjmuje tekst do szyfrowania
+     * i zwraca zakodowana wersje
+     *
+     * przyjmuje zakodowany tekst
+     * i zwraca rozkodowany
+     *
+     * */
+
+}
---
+++ src/test/java/sda/ciphers/CesarCipherTest.java
@@ -0,0 +1,49 @@
+package sda.ciphers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Szyfr Cezara - testy")
+class CesarCipherTest {
+
+    @DisplayName("When no arg constructor then empty decoded, encoded and base")
+    @Test
+    public void emptyArgConstructorReturnEmptyString() {
+        final CesarCipher cipher = new CesarCipher();
+        assertEquals("", cipher.getBase());
+        assertEquals("", cipher.getEncode());
+        assertEquals("", cipher.getDecoded());
+    }
+
+    @DisplayName("Check encode function")
+    @ParameterizedTest(name = "Test case {index} : input {0} gives result {1}")
+    @CsvSource(value = {"abc,def","xyz,abc", "xYz,aBc", "x Y z,aBc", "x Y!@$!$! z, aBc"})
+    public void getEncodeStringWhenGivenParamInput(
+            String entry, String result
+    ){
+        final CesarCipher cipher = new CesarCipher(entry);
+        cipher.encode();
+        String encode = cipher.getEncode();
+        assertEquals(result, encode);
+    }
+
+    @DisplayName("Check decode function")
+    @ParameterizedTest(name = "Test case {index} : input {0} gives result {1}")
+    @CsvSource(value = {"def,abc","abc,xyz", "aBc,xYz"})
+    public void getDecodeStringWhenGivenParamInput(
+            String entry, String result
+    ){
+        final CesarCipher cipher = new CesarCipher(entry);
+        cipher.decode();
+        String decoded = cipher.getDecoded();
+        assertEquals(result, decoded);
+    }
+
+
+
+
+}
