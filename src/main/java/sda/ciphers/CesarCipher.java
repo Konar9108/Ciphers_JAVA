6a46ddb5ab0038fb2dd488ca2130d62f9da6f0b5
Kamil Szestowicki <kszestowicki@logopak.de>
2020-04-19 13:17:37 +0200

    init

--- src/main/java/sda/ciphers/CesarCipher.java
+++ src/main/java/sda/ciphers/CesarCipher.java
@@ -1,59 +1,45 @@
 package sda.ciphers;
 
-public class CesarCipher implements ICipher {
+public class CesarCipher {
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
     private String base;
     private String encoded;
     private String decoded;
-    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public CesarCipher(String toEncode) {
+        this();
         base = toEncode;
     }
 
-
     public CesarCipher() {
         base = "";
-        encoded= "";
-        decoded= "";
+        encoded = "";
+        decoded = "";
     }
 
     public String getBase() {
         return base;
     }
 
-    public String getEncoded() {
-        return encoded;
-    }
-    public void decode() {
-        StringBuilder tempDecode = new StringBuilder();
-        for (int i = 0; i < base.length(); i++) {
-            char letter = base.charAt(i);
-            final boolean isLower = Character.isLowerCase(letter);
-            if (!isLower){
-                letter = Character.toLowerCase(letter);
-            }
-            final int position = alphabet.indexOf(letter);
-            final char c = alphabet.charAt(getDecodedPosition(position));
-            tempDecode.append(isLower ? c : Character.toUpperCase(c)); // pisanie if jako elvis, krocej
-        }
-        decoded = tempDecode.toString();
-
-    }
     public void encode() {
+        //a -> d
         StringBuilder tempEncode = new StringBuilder();
         for (int i = 0; i < base.length(); i++) {
             char letter = base.charAt(i);
 
-            if(!Character.isLetter(letter)) continue;
+            if (!Character.isLetter(letter)) continue;
 
             final boolean isLower = Character.isLowerCase(letter);
-            if (!isLower){
+            if (!isLower) {
                 letter = Character.toLowerCase(letter);
             }
             final int position = alphabet.indexOf(letter);
             final char c = alphabet.charAt(getEncodedPosition(position));
-            tempEncode.append(isLower ? c : Character.toUpperCase(c)); // pisanie if jako elvis, krocej
+
+            tempEncode.append(isLower ? c : Character.toUpperCase(c));
         }
+
+
         encoded = tempEncode.toString();
     }
 
@@ -61,25 +47,34 @@ public class CesarCipher implements ICipher {
         return (position + 3) % 26;
     }
 
+    public String getEncode() {
+        return encoded;
+    }
+
+    public void decode() {
+        //d -> a
+        StringBuilder tempEncode = new StringBuilder();
+        for (int i = 0; i < base.length(); i++) {
+            char letter = base.charAt(i);
+            final boolean isLower = Character.isLowerCase(letter);
+            if (!isLower) {
+                letter = Character.toLowerCase(letter);
+            }
+            final int position = alphabet.indexOf(letter);
+            final char c = alphabet.charAt(getDecodedPosition(position));
+
+            tempEncode.append(isLower ? c : Character.toUpperCase(c));
+        }
+
+
+        decoded = tempEncode.toString();
+    }
+
     private int getDecodedPosition(int position) {
-                return (position +23) % 26;
+        return (position + 23) % 26;
     }
 
     public String getDecoded() {
         return decoded;
     }
-
-
-
-
-//TODO
-    /*
-     * przyjmuje tekst do szyfrowania
-     * i zwraca zakodowana wersje
-     *
-     * przyjmuje zakodowany tekst
-     * i zwraca rozkodowany
-     *
-     * */
-
 }
--- src/test/java/sda/ciphers/CesarCipherTest.java
+++ src/test/java/sda/ciphers/CesarCipherTest.java
@@ -7,43 +7,50 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Szyfr Cezara - testy")
+@DisplayName("Cipher Cesar under test")
 class CesarCipherTest {
 
-    @DisplayName("When no arg constructor then empty decoded, encoded and base")
+    @DisplayName("When no arg constructor then empty encode string and empty decode string")
     @Test
     public void emptyArgConstructorReturnEmptyString() {
+        //given
         final CesarCipher cipher = new CesarCipher();
+        //when
+        //then
         assertEquals("", cipher.getBase());
-        assertEquals("", cipher.getEncoded());
+        assertEquals("", cipher.getEncode());
         assertEquals("", cipher.getDecoded());
     }
 
     @DisplayName("Check encode function")
     @ParameterizedTest(name = "Test case {index} : input {0} gives result {1}")
-    @CsvSource(value = {"abc,def","xyz,abc", "xYz,aBc", "x Y z,aBc", "x Y!@$!$! z, aBc"})
+    @CsvSource({"abc,def", "xyz,abc", "xYz,aBc", "x Y z,aBc","'',''"})
     public void getEncodeStringWhenGivenParamInput(
             String entry, String result
-    ){
+    ) {
+        //given
         final CesarCipher cipher = new CesarCipher(entry);
+        //when
         cipher.encode();
-        String encode = cipher.getEncoded();
+        String encode = cipher.getEncode();
+        //then
         assertEquals(result, encode);
+
     }
 
     @DisplayName("Check decode function")
     @ParameterizedTest(name = "Test case {index} : input {0} gives result {1}")
-    @CsvSource(value = {"def,abc","abc,xyz", "aBc,xYz"})
+    @CsvSource({"def,abc", "abc,xyz", "aBc,xYz"})
     public void getDecodeStringWhenGivenParamInput(
             String entry, String result
-    ){
+    ) {
+        //given
         final CesarCipher cipher = new CesarCipher(entry);
+        //when
         cipher.decode();
         String decoded = cipher.getDecoded();
+        //then
         assertEquals(result, decoded);
+
     }
-
-
-
-
 }
