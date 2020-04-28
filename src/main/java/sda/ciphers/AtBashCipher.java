894bf094b73ef277a853a8b8ba4dc65c2084880d
Damian Konarowski <konar9108@gmail.com>
2020-04-28 19:56:35 +0200

    added functional AtBashCipher

--- src/main/java/sda/ciphers/AtBashCipher.java
+++ src/main/java/sda/ciphers/AtBashCipher.java
@@ -1,24 +1,22 @@
 package sda.ciphers;
 
-public class AtBashCipher implements ICipher {
+public class AtBashCipher {
 
     private String base;
     private String encoded;
     private String decoded;
-    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public AtBashCipher(String base) {
         this.base = base;
+        encoded = "";
+        decoded = "";
     }
 
-    @Override
-    public void decode() {
 
-    }
-
-    @Override
-    public void encode() {
-
+    public AtBashCipher() {
+        base = "";
+        encoded = "";
+        decoded = "";
     }
 
     public String getEncoded() {
@@ -29,8 +27,42 @@ public class AtBashCipher implements ICipher {
         return decoded;
     }
 
-    @Override
     public String getBase() {
-        return null;
+        return base;
     }
+
+    public void encode() {
+
+        StringBuilder tempEncode = new StringBuilder();
+        int ascii;
+        int temp;
+        for (int i = 0; i < base.length(); i++) {
+            char letter = base.charAt(i);
+
+            if(Character.isWhitespace(letter)) tempEncode.append(letter);
+            if (!Character.isLetter(letter)) continue;
+            final boolean isLower = Character.isLowerCase(letter);
+            if (!isLower) {
+                letter = Character.toLowerCase(letter);
+            }
+            ascii = letter;
+            temp = ascii - 97;
+            ascii = 122 - temp;
+            letter = (char) ascii;
+            if (!isLower) {
+                letter = Character.toUpperCase(letter);
+            }
+            tempEncode.append(letter);
+        }
+        encoded = tempEncode.toString();
+
+    }
+
+    public void decode(){
+        encode();
+        decoded = encoded;
+
+    }
+
+
 }
--- src/test/java/sda/ciphers/AtBashCipherTest.java
+++ src/test/java/sda/ciphers/AtBashCipherTest.java
@@ -1,9 +1,36 @@
 package sda.ciphers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("szyfr AtBash - testy")
 class AtBashCipherTest {
 
 
+    @DisplayName("check if non arg constructor has empty bot not null fields")
+    @Test
+    public void emptyConstructorChecker(){
+        AtBashCipher atBashCipher = new AtBashCipher();
+        assertEquals("",atBashCipher.getDecoded());
+        assertEquals("",atBashCipher.getEncoded());
+        assertEquals("",atBashCipher.getBase());
 
+    }
+
+    //
+
+    @DisplayName("Check encode function")
+    @ParameterizedTest(name = "Test case {index} : input {0} gives result {1}")
+    @CsvSource(value = {"abc,zyx","xyz,cba", "xYz,cBa", "def,wvu", "Janusz kurcze ocb, Qzmfha pfixav lxy"})
+    public void getEncodedStringWhenGivenParamInput(String entry, String result) {
+        AtBashCipher atBashCipher = new AtBashCipher(entry);
+        atBashCipher.encode();
+        String encoded = atBashCipher.getEncoded();
+        assertEquals(result,encoded);
+
+    }
 }
